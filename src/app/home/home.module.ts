ce16f1a7b628a0801d0ddaab2fd51aba911e2e44
kirandash <thekirankumardash@gmail.com>
2018-02-05 23:31:48 +0530

    4. Create Module for 4 pages.

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeDetailComponent } from './home-detail/home-detail.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [HomeDetailComponent]
+})
+export class HomeModule { }
---
+++ src/app/service/service.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ServiceDetailComponent } from './service-detail/service-detail.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ServiceDetailComponent]
+})
+export class ServiceModule { }
