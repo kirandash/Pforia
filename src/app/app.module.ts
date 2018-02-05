09ce64d92c5acb01c856eba41331c9b2f7e7268d
kirandash <thekirankumardash@gmail.com>
2018-02-05 23:21:52 +0530

    2. Create Footer and sidebar component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BannerComponent
+    BannerComponent,
+    FooterComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
