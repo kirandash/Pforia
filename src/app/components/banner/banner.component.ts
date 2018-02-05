7862e11bcc079949b37b0691e1643b67a7528df6
kirandash <thekirankumardash@gmail.com>
2018-02-06 02:22:15 +0530

    10. Use ngStyle to add style rules to the DOM.

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./banner.component.css']
 })
 export class BannerComponent implements OnInit {
+  
+  bannerImage: string = 'assets/images/frontbanner.jpg';
 
   constructor() { }
 
