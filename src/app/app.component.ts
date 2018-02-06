7019d1a028959224bf9e84fb3d7db1bf2846aced
kirandash <thekirankumardash@gmail.com>
2018-02-07 01:14:49 +0530

    12. Module based banner image with router events and NavigationEnd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-  image: string = 'assets/images/frontbanner.jpg';
+  image: string;
+
+  constructor(private _router: Router){
+  	this.image = 'assets/images/front-banner.jpg';
+  	// console.log(_router.url);
+  	_router.events.subscribe((e) => {
+  		if(e instanceof NavigationEnd) {
+  			switch (e.url) {
+  				case "/home":
+  					// code...
+  					this.image = 'assets/images/front-banner.jpg';
+  					break;
+  				case "/gamme":
+  					this.image = 'assets/images/gamme-banner.jpg';
+  					break;
+  				case "/service":
+  					this.image = 'assets/images/service-banner.jpg';
+  					break;
+  				case "/contact":
+  					this.image = 'assets/images/contact-banner.jpg';
+  					break;
+  				default:
+  					// code...
+  					this.image = 'assets/images/front-banner.jpg';
+  					break;
+  			}
+  		}
+  	});
+  }
+
+
 }
