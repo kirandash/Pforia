3e66a5e4a0e9f3eee3cecd5796adc2de339bc252
kirandash <thekirankumardash@gmail.com>
2018-02-07 01:39:49 +0530

    13. jQuery smooth scroll with AfterViewInit, ElementRef, ViewChild

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -1,16 +1,27 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import * as $ from 'jquery';
+//http://deanmalone.net/post/using-jquery-from-angular2/
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.css']
 })
-export class BannerComponent implements OnInit {
+export class BannerComponent implements OnInit, AfterViewInit {
   @Input() bannerImage: string;
+  @ViewChild('scrollDown') el:ElementRef;
   
   constructor() { }
   
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+  	$(this.el.nativeElement).click(function(){
+  		$('html, body').animate({
+  			scrollTop: $('#primary').offset().top
+  		}, 1000);
+  		return false;
+  	});
+  }
 }
