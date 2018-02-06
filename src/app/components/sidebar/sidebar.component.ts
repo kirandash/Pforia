5d73df99e907602d07ca09cda968a0430c25a654
kirandash <thekirankumardash@gmail.com>
2018-02-07 02:01:18 +0530

    14 Add bootstrap and sidebar script in sidebar.component.ts

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-sidebar',
@@ -10,6 +11,26 @@ export class SidebarComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(){
+	$(window).scroll(function(){
+		var top = $(document).scrollTop();
+		var batas = $('.main-menu-wrap').height();
+		
+		if(top > batas) {
+			$('.unfixed-menu').addClass('show-fixed-menu');
+		} else {
+			$('.unfixed-menu').removeClass('show-fixed-menu');	
+		}
+
+		//Mobile Fixed Menu
+		if(top > 0) {
+			$('.site-branding.mobile, #page .main-menu-wrap.mobile-menu').addClass('fix-me');
+		}else {
+			$('.site-branding.mobile, #page .main-menu-wrap.mobile-menu').removeClass('fix-me');
+		}
+	});  	
   }
 
 }
